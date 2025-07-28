86dfadadb80b45310123a105e7bf343f0efd3eec
SakshamC12 <saksham1205@gmail.com>
2025-07-28 15:51:45 +0530

    deploy

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ktmpaddijvfhokucwnuy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0bXBhZGRpanZmaG9rdWN3bnV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0MTg2NDIsImV4cCI6MjA2ODk5NDY0Mn0.NWywC6jsp3Ayvvk7aMUF_XwQO-v-Pklh3-sSSgFIfcE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
